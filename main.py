f6b327f6d76d49856ffdee31d072f8f5d781a076
PhilemonVanDussen <vandussenph709@northwested.org>
2024-11-04 13:00:35 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,49 @@
+# for number in range(6):
+#     print(f'The number printed was: {number}')
+
+# for x in range(1, 21):
+#     print(f'The number printed was: {x}')
+
+# for x in range(1, 21, 5):
+#     print(f'The number printed was: {x}')
+
+
+
+# for x in range(1, 501):
+#     result = x * 10
+#     print(f'The number {x} multiplied by 10 = {result}')
+
+# students = ['Braden', 'Ethan', 'Matt', 'Trent', 'Dax']
+
+# for student in students:
+#     print(f'{student} in all uppercase characters is: {student.upper()}')
+
+#number_of_students = len(students) 
+
+# for num in range(len(students)):
+#     print(f'{students[num]} in all uppercase characters is: {students[num].upper()}')
+
+# for num in range(0, 1000001, 1000):
+#     print(num)
+
+
+
+# for num in range(1, 1000000):
+#     add = num + 1
+#     print(add)
+
+# for num in range(1, 21, 2):
+#     print(num)
+
+# for num in range(3, 31, 3):
+#     print(num)
+
+cubes = []
+for values in range(1, 11):
+    cubes.append(values ** 3)
+    
+print(cubes)
+
+
+
+
